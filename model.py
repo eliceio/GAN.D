d9aa8b60f395f01022bf77cdf5512c6a287dee24
Jeonghyo Kim <wjdgy3746@kaist.ac.kr>
2018-08-20 22:06:08 +0900

    encoder

--- model.py
+++ model.py
@@ -1,23 +1,33 @@
 import tensorflow as tf
 import numpy as np
 
-class Encoder:
+class AE_Model:
 
     def __init__(self):
         pass
 
-    def network(self):
-        pass
+    def Encoder(self):
+        x=tf.placeholder(self.input_shape)
+        x=tf.nn.relu(tf.layers.conv2d(x, 128, kernel_size=3, padding='same'))
+        x=tf.layers.max_pooling2d(x, pool_size=2)
+        x=tf.nn.relu(tf.layers.conv2d(x, 64, kernel_size=3, padding='same'))
+        x=tf.layers.max_pooling2d(x, pool_size=2)
+        x = tf.nn.relu(tf.layers.conv2d(x, 32, kernel_size=3, padding='same'))
+        x = tf.layers.max_pooling2d(x, pool_size=2)
+
+        shape=tf.shape(x)
+        x = tf.layers.flatten(x)
+        x = tf.layers.dense(x, 128)
+
+        z_mean = tf.layers.dense(x, self.latent_shape)
+        z = sampling(z_mean, z_log_var)
+
+        return z
 
 
-class Decoder:
-    def __init__(self):
-        pass
-
-    def network(self):
+    def Decoder(self):
         # Encoder
         # Decoder
-        
         pass
 
 
