bd1cbb664857fd9e77d5177d7a7ae7563ca71ae3
jinsu <dla0830ld@gmail.com>
2018-08-20 22:25:14 +0900

    add lstm birection

--- module.py
+++ module.py
@@ -1,20 +1,25 @@
 import tensorflow as tf
 
 
-def LSTM(inputs, num_unit, seqlens=None):
+def LSTM(inputs, num_unit, seqlens=None, bidirection=False):
     cell = tf.contrib.rnn.BasicLSTMCell(num_unit, forget_bias=1.0)
-    outputs, states = tf.nn.dynamic_rnn(cell, inputs,
-                                        sequence_length=seqlens,
-                                        dtype=tf.float32)
+
+    if (bidirection):
+        outputs, states = tf.nn.bidirectional_dynamic_rnn(cell, inputs,
+                                                          sequence_length=seqlens,
+                                                          dtype=tf.float32)
+    else:
+        outputs, states = tf.nn.dynamic_rnn(cell, inputs,
+                                            sequence_length=seqlens,
+                                            dtype=tf.float32)
 
     return outputs
 
 
 def Dropout(inputs, keep_prob):
-    dropout = tf.contrib.rnn.DropoutWrapper(
+    return tf.contrib.rnn.DropoutWrapper(
         inputs, output_keep_prob=keep_prob)
 
-    return dropout
 
 def Dense(inputs, num_unit, activation=tf.nn.relu):
     return tf.layers.dense(inputs, units=num_unit, activation=activation)
