296539678ce9b92643e764bc9e5a2f878c347079
kyle <kcs93023@gmail.com>
2018-08-20 22:03:36 +0900

    sampling, init 추가

--- model.py
+++ model.py
@@ -1,23 +1,27 @@
 import tensorflow as tf
 import numpy as np
 
-class Encoder:
+class AE_Model:
+    def __init__(self, latent_dim, batch_size):
+        self.latent_dim = latent_dim
+        self.batch_size = batch_size
+        self.input_shape = [256, 256, 1] #w * h * c
+        self.latent_shape = 128
+    
 
-    def __init__(self):
+    def sampling(z_mean,z_log_var):
+        z_mean= z_mean
+        z_log_var = z_log_var
+        batch = self.batch_size
+        dim = self.latent_shapeßS
+        epsilon = tf.random_normal(shape=(batch,dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+    def encoder(self):
         pass
 
-    def network(self):
-        pass
 
-
-class Decoder:
-    def __init__(self):
-        pass
-
-    def network(self):
-        # Encoder
-        # Decoder
-        
+    def decoder(self):s
         pass
 
 
