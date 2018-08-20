add1ba5d1173bc1c5048dc98d850dca6c28187a2
unknown <whgpals091@kaist.ac.kr>
2018-08-20 22:06:08 +0900

    decoder

--- model.py
+++ model.py
@@ -1,24 +1,29 @@
 import tensorflow as tf
 import numpy as np
 
-class Encoder:
+class AEmodel:
 
     def __init__(self):
         pass
 
-    def network(self):
+    def Encoder(self):
         pass
 
+    def Decoder(self):
+        latent_inputs = x
+        x = tf.layers.dense(latent_inputs, shape[1] * shape[2] * shape[3], activation='relu', kernel_initializer='glorot_uniform')
+        x = tf.reshape(x, [shape[1], shape[2], shape[3]])
+        x = tf.layers.dense(x, 128, kernel_initializer='glorot_uniform')
 
-class Decoder:
-    def __init__(self):
-        pass
+        x = tf.layers.conv2d(x, filters=32, kernel_size=3, activation='relu', padding='same')
+        x = tf.image.resize_images(x, size=[2, 2])
+        x = tf.layers.conv2d(x, filters=64, kernel_size=3, activation='relu', padding='same')
+        x = tf.image.resize_images(x, size=[2, 2])
+        x = tf.layers.conv2d(x, filters=128, kernel_size=3, activation='relu', padding='same')
+        x = tf.image.resize_images(x, size=[2, 2])
+        x = tf.layers.conv2d(x, filters=1, kernel_size=3, activation='sigmoid', padding='same')
+        return z
 
-    def network(self):
-        # Encoder
-        # Decoder
-        
-        pass
 
 
 class RNN_Model:
