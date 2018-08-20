9efc07961dca861c794e7253a9452c980b56ae25
jinsu <dla0830ld@gmail.com>
2018-08-20 21:02:28 +0900

    add model frame

--- model.py
+++ model.py
@@ -1,1 +1,26 @@
 import tensorflow as tf
+import numpy as np
+
+class Encoder:
+
+    def __init__(self):
+        pass
+
+    def network(self):
+        pass
+
+
+class Decoder:
+    def __init__(self):
+        pass
+
+    def network(self):
+        pass
+
+
+class DanceNet:
+    def __init__(self):
+        pass
+
+    def network(self):
+        pass
