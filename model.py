de8611ecc7ddd7062a57512e1c1a1a1f2fea36bc
jinsu <dla0830ld@gmail.com>
2018-08-21 17:06:30 +0900

    add Discriminator

--- model.py
+++ model.py
@@ -90,7 +90,7 @@ class TestModel:
 
         # make network
         self.network = tf.make_template('net', self._network)
-        self.logit = self.network()
+        self.G_out, self.D_real, self.D_fake = self.network()
 
     def _Encoder(self, inputs, reuse=False):
         with tf.variable_scope('Encoder', reuse=reuse):  # inputs <- (batch, 256, 256, c)
@@ -171,6 +171,7 @@ class TestModel:
 
             return pred_seq_vec
 
+    # todo : noise를 어떻게 넣어야 할까?, pred rnn 할때? 그떄 동작을 만드니까?
     def _Generator(self):
         with tf.variable_scope('Generator'):
             # encoding input image
@@ -182,21 +183,74 @@ class TestModel:
 
             # decoding seq_vec to image
             pred_shots = []
-            for i in range(self.pred_size):
+            for i in range(self.pred_size + 2):
                 pred_shots.append(self._Decoder(pred_seq_vec[:, i:i + 1, :], reuse=None if i == 0 else True))
 
-            return pred_shots
+            return pred_shots  # (pred_size + 2, ) return list
 
-    def _Discriminator(self):
-        pass
+    def _Discriminator(self, inputs, reuse=False):
+        with tf.variable_scope('Discriminator', reuse=reuse):
+            def encoder_dis(input, reuse=False):  # todo : pix2pix의 Discriminator 처럼하면 어떨까?
+                with tf.variable_scope('Encoder_Dis', reuse=reuse):
+                    enc = tf.layers.conv2d(input, 128, kernel_size=3, activation=tf.nn.relu,
+                                           padding='same')  # (batch, 25, 256, 128)
+                    enc = tf.layers.max_pooling2d(enc, pool_size=2, strides=2)  # (batch, 128, 128, 128)
+                    enc = tf.layers.conv2d(enc, 64, kernel_size=3, activation=tf.nn.relu,
+                                           padding='same')  # (batch, 128, 128, 64)
+                    enc = tf.layers.max_pooling2d(enc, pool_size=2, strides=2)  # (batch, 64, 64, 64)
+                    enc = tf.layers.conv2d(enc, 32, kernel_size=3, activation=tf.nn.relu,
+                                           padding='same')  # (batch, 64, 64, 32)
+                    enc = tf.layers.max_pooling2d(enc, pool_size=2, strides=2)  # (batch, 32, 32, 32)
+                    enc = tf.layers.flatten(enc)
+                    enc = tf.layers.dense(enc, 128)  # (batch, 128)
 
+                    enc = tf.expand_dims(enc, 1)  # expand dim
+                return enc
+
+            encoded_inputs = []
+            for i in range(self.pred_size + 2):
+                encoded_inputs.append(encoder_dis(inputs[i], reuse=None if i == 0 else True))
+
+            encoded_inputs = tf.concat(encoded_inputs, axis=1)
+
+            # Bi-LSTM
+            with tf.variable_scope('Bi_LSTM'):
+                lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(256, forget_bias=1.0)
+                lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(256, forget_bias=1.0)
+                (output_fw, output_bw), states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell,
+                                                                                 encoded_inputs,
+                                                                                 dtype=tf.float32)  # (batch, pred_size + 2, 256) x2
+                outputs = tf.concat([output_fw, output_bw], axis=2)  # (batch, pred_size + 2, 512)
+                outputs = tf.layers.dense(outputs, 128, activation=tf.nn.tanh)  # (batch, pred_size + 2, 128)
+
+            # GRU recurrent
+            with tf.variable_scope('GRU'):
+                cell_fw = tf.contrib.rnn.GRUCell(64)
+                cell_bw = tf.contrib.rnn.GRUCell(64)
+                gru_ouputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, outputs, dtype=tf.float32)
+                gru_ouputs = tf.concat(gru_ouputs, 2)
+
+            ## Disciriminator output
+            out = gru_ouputs[:, -1, :]
+            W_dis = tf.get_variable("weights", shape=[128, 1])
+            b_dis = tf.get_variable("bias", shape=[1])
+            out = tf.sigmoid(tf.matmul(out, W_dis) + b_dis)
+
+            return out
+
+    # todo : Generator의 출력을 target 처럼 나오도록 수정하고 Discriminator도 맞도록 수정
     def _network(self):
         G_out = self._Generator()
-        D_real = self._Discriminator()
+        D_real = None # 나중에 추가 할 것
         D_fake = self._Discriminator(G_out)
 
-    def __call__(self, *args, **kwargs):
+        return G_out, D_real, D_fake
+
+    def discriminator_train_op(self):
         pass
 
-    def loss(self):
+    def generator_train_op(self):
         pass
+
+    def __call__(self, *args, **kwargs):
+        return None
