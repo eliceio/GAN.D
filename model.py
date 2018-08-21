1d7548c45024ac0223e727c16db93974c6e783ed
pkh091 <41953285+pkh091@users.noreply.github.com>
2018-08-21 17:22:16 +0900

    Update model.py

--- model.py
+++ model.py
@@ -35,18 +35,18 @@ class AE_Model:
         z = self.sampling(z_mean, z_mean)
 
         latent_inputs = z
-        x = tf.layers.dense(latent_inputs, shape[1] * shape[2] * shape[3], activation='relu',
-                            kernel_initializer='glorot_uniform')
+        x = tf.nn.relu(tf.layers.dense(latent_inputs, shape[1] * shape[2] * shape[3],
+                            kernel_initializer='glorot_uniform'))
         x = tf.reshape(x, [shape[1], shape[2], shape[3]])
         x = tf.layers.dense(x, 128, kernel_initializer='glorot_uniform')
-
-        x = tf.layers.conv2d(x, filters=32, kernel_size=3, activation='relu', padding='same')
-        x = tf.image.resize_images(x, size=[2, 2])
-        x = tf.layers.conv2d(x, filters=64, kernel_size=3, activation='relu', padding='same')
-        x = tf.image.resize_images(x, size=[2, 2])
-        x = tf.layers.conv2d(x, filters=128, kernel_size=3, activation='relu', padding='same')
-        x = tf.image.resize_images(x, size=[2, 2])
-        x = tf.layers.conv2d(x, filters=1, kernel_size=3, activation='sigmoid', padding='same')
+   
+        x = tf.nn.relu(tf.layers.conv2d(x, filters=32, kernel_size=3, padding='same'))
+        x = tf.image.resize_images(x, size=[2*shape[1], 2*shape[2]])
+        x = tf.nn.relu(tf.layers.conv2d(x, filters=64, kernel_size=3, padding='same'))
+        x = tf.image.resize_images(x, size=[2*shape[1], 2*shape[2]])
+        x = tf.nn.relu(tf.layers.conv2d(x, filters=128, kernel_size=3, padding='same'))
+        x = tf.image.resize_images(x, size=[2*shape[1], 2*shape[2]])
+        x = tf.nn.sigmoid(tf.layers.conv2d(x, filters=1, kernel_size=3, padding='same'))
         return z
 
 
