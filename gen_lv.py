1985a8336a208475cdcaf54c28840a77f670c47c
kcs93023 <kcs93023@gmail.com>
2018-09-01 20:11:22 +0900

    Edit gen_lv.py에 우리 데이터를 먹일 수 있도록 변경

--- gen_lv.py
+++ gen_lv.py
@@ -4,23 +4,29 @@ import os
 import numpy as np
 import cv2
 from model import AE_Model
-
+from ops import image_list
 
 def main():
     lv_array = []
     latent_dim = 128
     batch_size = 1
     logdir = './logdir/{}'.format("AE_1/")
-    limit = len(os.listdir('imgs'))
     model = AE_Model(latent_dim, batch_size)
 
+    # make images flat
+    imgs = []
+    for i in len(image_list):
+        for img in image_list[i] :
+            imgs.append(img)
+    # imgs = np.array(imgs)
     with tf.Session() as sess:
         model.load(sess, logdir=logdir)
-        for i in range(1, limit):
-            img = cv2.imread('imgs/{}.jpg'.format(i), cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img, (208, 120))
+        for img in imgs:
+            # img = cv2.imread('imgs/{}.jpg'.format(i), cv2.IMREAD_GRAYSCALE)
+            # img = cv2.resize(img, (208, 120))
             data_np = np.array(img) / 255
-            data_np = data_np.reshape(1, 120, 208, 1)
+            # at now, image's dimension (256x256x1)
+            data_np = data_np.reshape(1, 256, 256, 1)
             lv = sess.run(model.encoder_pred, feed_dict={model.input_image: data_np})
             lv = np.resize(lv, (latent_dim,))
             lv_array.append(lv)
